219f3197a2adececb1a48d4635ffeebcb56e11ad
James Costa <130270571+JAMESCOSTA01@users.noreply.github.com>
2023-10-11 23:32:31 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,14 @@
+nomes = ["Sophia", "Benício", "Carolina", "James"]
+nomes.append("Joel")
+nomes.insert(0, "Hamiton")
+nomes [1] = "Benizinho"
+#So = nomes.pop(0)
+print("Te amo + .title()+ "."")
+print ("Olá," + " " + nomes[0].title())
+print ("Hello," + " " + nomes[1])
+print ("Oi," + " " + nomes[2])
+print ("Hey," +" " + nomes[3])
+print ("Olá, " + " " + nomes[4])
+print (nomes[-2])
+mensagem = "O nome da minha esposa é " + nomes[-2] + "." 
+print (mensagem)
